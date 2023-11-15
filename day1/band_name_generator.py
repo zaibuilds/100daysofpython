aa29eb2a603ccbe0a63527e19c65a1a9e85e4c82
Zai Builds <hello@zaibuilds.io>
2023-11-15 00:19:49 +0000

    refactoring

--- day1/band_name_generator.py
+++ day1/band_name_generator.py
@@ -1,1 +1,4 @@
-print("Your band name could be " + input("What is your birth month?\n") + " " + input("What is your favourite animal?\nNo "))
+birth_month = input("What is your birth month?\n");
+favourite_animal = input("What is your favourite animal?\n");
+
+print("Your band name could be " + birth_month + " " + favourite_animal)
